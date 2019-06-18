547807fda8223b859507a4dea37a28af2a40eb61
root <root@k8s-master.xemolab.com>
2019-06-18 13:57:13 +0530

    modified:   code27.py

--- code27.py
+++ code27.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 for i in range(9):
 	for j in range(9):
 		if( (i - j < 5) & (j - i < 5 ) &  (i + j > 3) & (i + j < 13)  ):
-			print( "*",  end=" ")
+			print( "#",  end=" ")
 		else:
 			print (" ", end=" ")
 	print("")
