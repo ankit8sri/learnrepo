ddc33ce6e901576fe1c95288a3fb3ae3cadf81fb
root <root@k8s-master.xemolab.com>
2019-06-18 12:56:02 +0530

    python test code for container

---
+++ code27.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+for i in range(9):
+	for j in range(9):
+		if( (i - j < 5) & (j - i < 5 ) &  (i + j > 3) & (i + j < 13)  ):
+			print( "*",  end=" ")
+		else:
+			print (" ", end=" ")
+	print("")
